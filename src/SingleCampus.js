51b7f61869cef41df09d94c227879b021d04e070
mmartin2755@gmail.com <mmartin2755@gmail.com>
2021-04-05 19:40:56 -0400

    git version issue fixed

--- src/SingleCampus.js
+++ src/SingleCampus.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { connect } from "react-redux";
-<<<<<<< HEAD
 import { getCampuses, getCampus } from "../store";
 
 class SingleCampus extends React.Component {
@@ -24,88 +23,9 @@ class SingleCampus extends React.Component {
 
 const mapState = (state) => {
   console.log(state);
-=======
-import { deleteCampus } from "../store";
-
-const SingleCampus = ({ campus, deleteCampus }) => {
-  if (!campus) {
-    return null;
-  }
-  return (
-    <div>
-      {campus.name}
-      <button onClick={() => deleteCampus(campus)}> Delete This Campus</button>
-    </div>
-  );
-};
-
-const mapStateToProps = (state, otherProps) => {
-  const campus =
-    state.campuses.find(
-      (campus) => campus.id === otherProps.match.params.id * 1
-    ) || {};
->>>>>>> 9d26ebceef1f1662aa84756808cf4473abd85e20
   return {
     campus,
   };
 };
-<<<<<<< HEAD
 
 export default connect(mapState)(SingleCampus);
-=======
-
-// class SingleCampus extends React.Component {
-//     constructor(){
-
-//     }
-//   componentDidMount() {
-//     this.props.loadCampuses();
-//     console.log(this.props);
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         {this.props.match.params.id}
-//         <ul>
-//           {this.props.campuses
-//             .map((campus) => {
-//               return (
-//                 <li key={campus.id}>
-//                   {campus.name}
-//                   <img
-//                     className="logo"
-//                     height="100"
-//                     width="100"
-//                     src={campus.imageURL}
-//                   ></img>
-//                 </li>
-//               );
-//             })
-//             .filter((campus) => campus.id === this.props.match.params.id)}
-//         </ul>
-//         <div>details </div>
-//       </div>
-//     );
-//   }
-// }
-
-// const mapState = (state) => {
-//   return {
-//     campuses: state.campuses,
-//   };
-// };
-// const mapDispatchToProps = (dispatch) => {
-//   return {
-//     loadCampuses: () => dispatch(getCampuses()),
-//   };
-// };
-
-const mapDispatchToProps = (dispatch, { history }) => {
-  return {
-    deleteCampus: (campus) => dispatch(deleteCampus(campus, history)),
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(SingleCampus);
->>>>>>> 9d26ebceef1f1662aa84756808cf4473abd85e20
